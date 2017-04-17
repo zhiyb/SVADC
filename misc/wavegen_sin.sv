0c078b7f4554a1f59799e18e55f18f4ae08ec507
Norman Zhi <normanzyb@gmail.com>
2017-04-17 06:01:05 +0100

    Waveform generator sin PWM fix

--- misc/wavegen_sin.sv
+++ misc/wavegen_sin.sv
@@ -4,11 +4,8 @@ module wavegen_sin #(parameter N = 4, SN = 5) (
 );
 
 logic [SN - 1:0] addr;
-logic [N - 1:0] fifo, data;
-rom_sin_4_5 rom0 (addr, clk, fifo);
-
-always_ff @(posedge clk)
-	data <= fifo;
+logic [N - 1:0] rom;
+rom_sin_4_5 rom0 (addr, clk, rom);
 
 logic [N - 1:0] cnt;
 always_ff @(posedge clk, negedge n_reset)
@@ -20,13 +17,13 @@ always_ff @(posedge clk, negedge n_reset)
 always_ff @(posedge clk, negedge n_reset)
 	if (~n_reset)
 		addr <= 0;
-	else if (cnt == {N - 1{1'b1}})
+	else if (cnt == {{N - 2{1'b1}}, 2'b01})
 		addr <= addr + 1;
 
 logic match, reload;
 always_ff @(posedge clk)
 begin
-	match <= cnt == data;
+	match <= cnt == rom;
 	reload <= cnt == 0;
 end
 
